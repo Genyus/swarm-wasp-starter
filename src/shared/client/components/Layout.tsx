3e979210d555f6e3354e5c26da30b0af36974214
Genyus <genyus@gmail.com>
2025-10-19 23:30:22 -0400

    refactor: use shadcn sonner component

--- src/shared/client/components/Layout.tsx
+++ src/shared/client/components/Layout.tsx
@@ -1,24 +1,16 @@
 import { Outlet } from "react-router-dom";
-import { Toaster } from "sonner";
 import { useThemeProvider } from "../hooks/useTheme";
 import "../Main.css";
-import { TooltipProvider } from "./ui";
+import { Toaster, TooltipProvider } from "./ui";
 
 // TODO: Add things like a header and footer
-// TooltipProvider and ThemeContext are already included.
-
 export default function Layout() {
   const ThemeProvider = useThemeProvider();
 
   return (
     <ThemeProvider>
       <TooltipProvider>
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            className: "bg-surface-800 text-surface-100 border",
-          }}
-        />
+        <Toaster position="top-center" />
         <Outlet />
       </TooltipProvider>
     </ThemeProvider>
