d03bad887bdf66b270d202754bcacbceea4cd284
Genyus <genyus@gmail.com>
2025-11-17 01:14:14 -0400

    chore: bump Wasp version to 0.18.2

--- main.wasp.ts
+++ main.wasp.ts
@@ -2,7 +2,7 @@ import { App } from "@ingenyus/swarm-wasp";
 
 const app = await App.create("swarm-wasp-starter", {
   title: "Swarm Wasp Starter",
-  wasp: { version: "^0.18.1" },
+  wasp: { version: "^0.18.2" },
   head: [
     '<meta name="description" content="Swarm Wasp Starter description">',
     '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
